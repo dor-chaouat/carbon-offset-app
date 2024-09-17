709cac18bb7e56db924c8e9cfb495cacbffdaa49
dor-chaouat <dorch@wix.com>
2024-09-17 21:05:56 +0300

    wip

--- src/backend/events/installation/event.ts
+++ src/backend/events/installation/event.ts
@@ -3,47 +3,61 @@ import { collections } from '@wix/data';
 import { appInstances } from '@wix/app-management';
 import { CHECKOUT_COLLECTION_ID, SETTINGS_COLLECTION_ID } from '../../consts';
 
-appInstances.onAppInstanceInstalled(() => {
-  auth.elevate(collections.createDataCollection)({
-    _id: SETTINGS_COLLECTION_ID,
-    displayName: "Carbon Offset Settings",
-    fields: [
-      { key: 'title', type: collections.Type.TEXT },
-      { key: 'amount', type: collections.Type.NUMBER },
-      { key: 'color', type: collections.Type.TEXT },
-      { key: 'iconColor', type: collections.Type.TEXT },
-    ],
-    permissions: {
-      // Make sure to change the permissions according to the actual usage of your collection
-      insert: collections.Role.ANYONE,
-      read: collections.Role.ANYONE,
-      remove: collections.Role.ANYONE,
-      update: collections.Role.ANYONE,
-    },
-    // Plugin for single item collection
-    plugins: [{
-      type: collections.PluginType.SINGLE_ITEM,
-      singleItemOptions: {
-        singleItemId: "SETTINGS"
+appInstances.onAppInstanceInstalled(async () => {
+  console.log("CREATING COLLECTIONS");
+  try {
+    const res = await auth.elevate(collections.createDataCollection)({
+      _id: SETTINGS_COLLECTION_ID,
+      displayName: "Carbon Offset Settings",
+      fields: [
+        { key: 'title', type: collections.Type.TEXT },
+        { key: 'amount', type: collections.Type.NUMBER },
+        { key: 'color', type: collections.Type.TEXT },
+        { key: 'iconColor', type: collections.Type.TEXT },
+      ],
+      permissions: {
+        // Make sure to change the permissions according to the actual usage of your collection
+        insert: collections.Role.ANYONE,
+        read: collections.Role.ANYONE,
+        remove: collections.Role.ANYONE,
+        update: collections.Role.ANYONE,
       },
-    }],
-  });
+      // Plugin for single item collection
+      plugins: [{
+        type: collections.PluginType.SINGLE_ITEM,
+        singleItemOptions: {
+          singleItemId: "SETTINGS"
+        },
+      }],
+    });
 
-  auth.elevate(collections.createDataCollection)({
-    _id: CHECKOUT_COLLECTION_ID,
-    displayName: "Carbon Offset Checkout",
-    fields: [
-      // In this case, checkoutId is stored as an "added" field that is not neccessarry
-      // the actual _id for each item of this collection will be purchaseFlowId for easy fetching
-      { key: 'checkoutId', type: collections.Type.TEXT },
-      { key: 'shouldAdd', type: collections.Type.BOOLEAN },
-    ],
-    permissions: {
-      // Make sure to change the permissions according to the actual usage of your collection
-      insert: collections.Role.ANYONE,
-      read: collections.Role.ANYONE,
-      remove: collections.Role.ANYONE,
-      update: collections.Role.ANYONE,
-    },
-  });
+    console.log("SETTINGS COLLECTION RESPONSE", res)
+  } catch (error) {
+    console.log("SETTINGS COLLECTION ERROR", error)
+  }
+
+
+  try {
+    const res = await auth.elevate(collections.createDataCollection)({
+      _id: CHECKOUT_COLLECTION_ID,
+      displayName: "Carbon Offset Checkout",
+      fields: [
+        // In this case, checkoutId is stored as an "added" field that is not neccessarry
+        // the actual _id for each item of this collection will be purchaseFlowId for easy fetching
+        { key: 'checkoutId', type: collections.Type.TEXT },
+        { key: 'shouldAdd', type: collections.Type.BOOLEAN },
+      ],
+      permissions: {
+        // Make sure to change the permissions according to the actual usage of your collection
+        insert: collections.Role.ANYONE,
+        read: collections.Role.ANYONE,
+        remove: collections.Role.ANYONE,
+        update: collections.Role.ANYONE,
+      },
+    });
+    
+    console.log("CHECKOUT COLLECTION RES", res)
+  } catch (error) {
+    console.log("CHECKOUT COLLECTION ERROR", error)
+  }
 });
